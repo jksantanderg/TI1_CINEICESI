b82d2ec3c75135eda0d8f07621cb5ac69aa75eb9
Yessica Santander Galvis <ysantandergalvis@gmail.com>
2022-03-14 21:56:50 -0500

    ajustes

--- src/control/Entry.java
+++ src/control/Entry.java
@@ -11,8 +11,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.Main;
+import model.Cinema;
+import model.CinemaData;
+
+
 
 public class Entry implements Initializable{
+	public Cinema cinema;
 
 	/**
 	 * this method directs you to the Event window where you can select your seat in the function
@@ -33,14 +38,12 @@ public class Entry implements Initializable{
 	 
 
 	 /**
-		 * this method directs you to the RegisterFunctios window where you can select your seat in the function
-		 * the method initialize 
-		 * @param event
-		 * @throws IOException
-		 */	
-	 
-	@FXML
-	
+	  * this method directs you to the RegisterFunctios window where you can select your seat in the function
+	  * the method initialize 
+	  * @param event
+	  * @throws IOException
+	  */		 
+	@FXML	
     void RegisterFuntions(ActionEvent event)  throws IOException {
     	FXMLLoader loader = new FXMLLoader(Main.class.getResource("../ui/RegisterFunctions.fxml"));
 		//loader.setController(new RegisterFunctions());
@@ -50,7 +53,28 @@ public class Entry implements Initializable{
 		stage.setScene(scene);
 		stage.show();
     }
+	
+	 /**
+		 * this method prints the function with the list of students enrolled in the event
+		 * @param event
+		 * 
+		 */		 
+	@FXML	
+   void print(ActionEvent event)  {
+		try{
+			System.out.println(cinema.getMovie() +"\n");
+			for(Cinema c : CinemaData.cinemas) {
+				System.out.println("\n"+
+				cinema.getStudents()+ "\n");
+		
+			}	
 
+		}catch(Exception ex){
+			System.out.println("Aun no a registrado un evento");
+		}
+		
+   }
+	
 	/**
 	 *the method initialize
 	 * @param location
